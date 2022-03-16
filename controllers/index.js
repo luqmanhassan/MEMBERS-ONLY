bc17aaf2ab5fdaa695cabbccf85773884c387a29
Luqman Hassan <reallookmanlook@gmail.com>
2022-03-16 03:43:30 -0700

    Implemented Express validation

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,8 @@ const UserModel = require('../models/user.js');
 const MessageModel = require('../models/message');
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
+const {body, validationResult} = require('express-validator');
+
 // Home
 const Home = function (req, res, next) {
   MessageModel.find()
@@ -16,35 +18,49 @@ const signupGetController = function (req, res, next) {
   res.render('signup', {title: 'Sign Up'});
 };
 
-const signUpPostController = function (req, res, next) {
-  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-    if (err) {
-      next(err);
+const signUpPostController = [
+  body('username', 'Empty name').trim().isLength({min: 1}).escape(),
+  body('password').isLength({min: 1}).escape(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const alert = errors.array();
+      res.render('signup', {title: 'Sign Up', alert});
     }
-    const item = new UserModel({
-      username: req.body.username,
-      password: hashedPassword,
-      avatar: req.body.avatar,
-      isMember: 'false',
-      isAdmin: 'false',
-    })
-      .save()
-      .then((result) => console.log(result))
-      .catch((err) => next(err));
-  });
-  res.redirect('/');
-};
+
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+      if (err) {
+        next(err);
+      }
+      const item = new UserModel({
+        username: req.body.username,
+        password: hashedPassword,
+        avatar: req.body.avatar,
+        isMember: 'false',
+        isAdmin: 'false',
+      })
+        .save()
+        .then((result) => console.log(result))
+        .catch((err) => next(err));
+    });
+    res.redirect('/');
+  },
+];
 
 // Login & Log Out
 const loginGetController = function (req, res, next) {
   res.render('login', {title: 'Login'});
 };
 
-const loginPostController = passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureMessage: true,
-});
+const loginPostController = [
+  body('username', 'Empty name').trim().isLength({min: 1}).escape(),
+  body('password').isLength({min: 1}).escape(),
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureMessage: true,
+  }),
+];
 
 const logOutController = (req, res) => {
   req.logout();
@@ -56,14 +72,24 @@ const messageGetController = function (req, res, next) {
   res.render('message', {title: 'Message'});
 };
 
-const messagePostController = function (req, res, next) {
-  const item = new MessageModel(req.body)
-    .save()
-    .then((result) => console.log(result))
-    .catch((err) => next(err));
+const messagePostController = [
+  body('title', 'Empty title').trim().isLength({min: 1}).escape(),
+  body('message', 'Empty message').trim().isLength({min: 1}).escape(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const alert = errors.array();
+      res.render('message', {title: 'Message', alert});
+    }
 
-  res.redirect('/');
-};
+    const item = new MessageModel(req.body)
+      .save()
+      .then((result) => console.log(result))
+      .catch((err) => next(err));
+
+    res.redirect('/');
+  },
+];
 
 // Member
 
@@ -71,18 +97,27 @@ const memberGetController = function (req, res, next) {
   res.render('member', {title: 'Member'});
 };
 
-const memberPostController = function (req, res, next) {
-  const id = req.body.id;
-  if (req.body.isMember === 'ape') {
-    UserModel.findByIdAndUpdate(id, {
-      isMember: 'true',
-    })
-      .then((result) => console.log(result))
-      .catch((err) => console.log(err));
-  }
+const memberPostController = [
+  body('isMember', 'Empty Value').trim().isLength({min: 3}).escape(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const alert = errors.array();
+      res.render('member', {title: 'Member', alert});
+    }
 
-  res.redirect('/');
-};
+    const id = req.body.id;
+    if (req.body.isMember === 'ape') {
+      UserModel.findByIdAndUpdate(id, {
+        isMember: 'true',
+      })
+        .then((result) => console.log(result))
+        .catch((err) => console.log(err));
+    }
+
+    res.redirect('/');
+  },
+];
 
 // Admin
 
@@ -90,18 +125,27 @@ const adminGetController = function (req, res, next) {
   res.render('admin', {title: 'Admin'});
 };
 
-const adminPostController = function (req, res, next) {
-  const id = req.body.id;
-  if (req.body.isAdmin === 'ape') {
-    UserModel.findByIdAndUpdate(id, {
-      isAdmin: 'true',
-    })
-      .then((result) => console.log(result))
-      .catch((err) => console.log(err));
-  }
+const adminPostController = [
+  body('isAdmin', 'Empty Value').trim().isLength({min: 3}).escape(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const alert = errors.array();
+      res.render('admin', {title: 'Admin', alert});
+    }
 
-  res.redirect('/');
-};
+    const id = req.body.id;
+    if (req.body.isAdmin === 'ape') {
+      UserModel.findByIdAndUpdate(id, {
+        isAdmin: 'true',
+      })
+        .then((result) => console.log(result))
+        .catch((err) => console.log(err));
+    }
+
+    res.redirect('/');
+  },
+];
 
 const deleteMessage = function (req, res, next) {
   const id = req.params.message;
